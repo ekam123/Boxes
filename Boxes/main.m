ca8bd85cdf3d0277c6f5323b05fe995af91cf28d
Ekam Singh Dhaliwal <ekamsingh123.es@gmail.com>
2019-05-28 12:31:04 -0700

    added method to calculate number boxes of box1 that can fit inside box2

--- Boxes/main.m
+++ Boxes/main.m
@@ -12,8 +12,14 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Box *box1 = [[Box alloc] initWithHeight:2 andWidth:3 andLength:6];
-        float volume = [box1 getVolume];
-        NSLog(@"%f", volume); 
+        NSLog(@"%f",[box1 getVolume]);
+        
+        Box *box2 = [[Box alloc] initWithHeight:2 andWidth:3 andLength:12];
+        NSLog(@"%f", [box2 getVolume]);
+        
+        int numberOfFits = [box1 numberOfBoxesCanFit:box2];
+        NSLog(@"%d", numberOfFits);
+        
             
     }
     return 0;
